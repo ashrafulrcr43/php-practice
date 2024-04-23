b4178480b22f3aac70c8847bacd196e9bc45b879
ashrafulrcr43 <124384576+ashrafulrcr43@users.noreply.github.com>
2024-04-23 21:42:59 +0600

    update

---
+++ Practice_2.php
@@ -0,0 +1,24 @@
+// if else else if:
+==================
+<?php 
+$score = 40;
+
+if($score >=80 && $score <100){
+echo "A+";
+}else if ($score >=70 && $score <80){
+    echo "A";
+}else if ($score >=60 && $score <70){
+    echo "A-";
+}else if ($score >=50 && $score <60){
+    echo "B";
+}else if ($score >=40 && $score <50){
+    echo "C";
+}else if ($score >=33 && $score <40){
+    echo "D";
+}else {
+    echo " F";
+}
+//nested if else:
+===================
+  
+
