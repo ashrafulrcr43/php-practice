8231e8111a2e332859707e53c0e1e1c04fcc5d07
ashrafulrcr43 <124384576+ashrafulrcr43@users.noreply.github.com>
2024-04-25 11:06:26 +0600

    update

--- Practice_2.php
+++ Practice_2.php
@@ -20,5 +20,65 @@ echo "A+";
 }
 //nested if else:
 ===================
-  
+<?php 
+$score = 60;
+$gread = '';
+if($score >=80){
+     $gread = 'A+';
+}else {
+     if($score >=70){
+          $gread = 'A';
+     } else {
+          if($score >=60){
+               $gread = 'A-';
+          }else {
+               if($score >=50){
+                    $gread = 'B';
+               }else {
+                    if($score >=40){
+                         $gread = 'C';
+                    }else {
+                         if($score >=33){
+                              $gread = 'D';
+                         }else {
+                              $gread = 'F';
+                         }
+                    }
+               }
+          }
+     }
+}
+printf($gread);
+
+
+
+//switch case
+====================
+    $x = 2;
+
+switch ($x) {
+
+ case 1:
+
+      echo "One";
+
+      break;
+
+ case 2:
+
+      echo "Two";
+
+      break;
+
+ case 3:
+
+      echo "Three";
+
+      break;
+
+ default:
+
+      echo "None";
+
+}
 
